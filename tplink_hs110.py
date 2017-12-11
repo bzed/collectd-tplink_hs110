34291f2a5d62923f3a20a7e2090f3e87e8790f4e
Bernd Zeimetz <bernd@bzed.de>
2017-12-12 00:51:23 +0100

    Update tplink_hs110.py

--- tplink_hs110.py
+++ tplink_hs110.py
@@ -1,24 +1,7 @@
 """
 Collect power usage data from TP-Link HS110 SmartPlugs.
-Requires pyHS100 version 0.2.4.2 (or a collectd build using Python3).
-   pip install pyHS100==0.2.4.2
 
-Drop tplink_hs110.py into /usr/share/collectd/python and add
-the following snippet to your collectd.conf:
-
-LoadPlugin python
-<Plugin python>
-        ModulePath "/usr/share/collectd/python"
-        LogTraces true
-        Import "tplink_hs110"
-        <Module tplink_hs110>
-                cr10s4 "10.11.12.13"
-                coffeemachine "10.11.12.1"
-        </Module>
-</Plugin>
-
-
-    Copyright (C) 2017 Bernd Zeimetz <bernd@bzed.de>
+    Copyright (C) 2017 Bernd Zeimetz <bernd@bzed.de
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
